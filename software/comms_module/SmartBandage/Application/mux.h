d51907fa62605de402637a407506650726638390
Mobius5150 <mike@michaelblouin.ca>
2016-02-09 20:19:53 -0700

    Adding barebones MUX interfaces
    See #13

---
+++ software/comms_module/SmartBandage/Application/mux.h
@@ -0,0 +1,29 @@
+/*
+ * @file mux.h
+ * @brief Contains external MUX interface definitions.
+ *
+ *  Created on: Feb 9, 2016
+ *      Author: michaelblouin
+ */
+
+#ifndef APPLICATION_MUX_H_
+#define APPLICATION_MUX_H_
+
+typedef enum {
+	S0 = 0,
+	S1 = 1,
+	S2 = 2,
+} MUX_SELECT;
+
+typedef enum {
+	Y0 = 0,
+	Y1 = 1,
+	Y2 = 2,
+	Y3 = 3,
+	Y4 = 4,
+	Y5 = 5,
+	Y6 = 6,
+	Y7 = 7,
+} MUX_OUTPUT;
+
+#endif /* APPLICATION_MUX_H_ */
